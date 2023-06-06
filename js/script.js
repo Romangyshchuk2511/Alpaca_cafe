30ac093fd917f30f73968c708146940c2d06bcc9
RomanGyshchuk <101799783+Romangyshchuk2511@users.noreply.github.com>
2023-06-06 18:19:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const chk = document.getElementById('chk');
+let header = document.querySelector('header');
+let nav = document.querySelectorAll('a');
+let container = document.querySelector('.container')
+let text = document.querySelectorAll('p');
+let main = document.querySelector('main');
 
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
+    container.classList.toggle('container-dark');
+    for(texts of text){
+         texts.classList.toggle('p-dark');
+    }   
+    header.classList.toggle('header-dark');
+    for(navs of nav){
+        navs.classList.toggle('a-dark');
+    }
+     main.classList.toggle('main-dark');
+     for(texts of text){
+        texts.classList.toggle('p-dark');
+    }
+    
 });
+
 let upButton = document.querySelector('.up-button');
 
 window.onscroll = function (){
